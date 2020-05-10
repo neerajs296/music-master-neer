a9bed817a3daf8168c6d8a8d55e7e6ef8e990cb0
Neeraj <neerajs296@gmail.com>
2020-05-10 22:53:49 +0530

    Updates

---
+++ precache-manifest.e497d8807f74445cdbeaf01ee26572e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1bfeae0c9d7a6e4f8be25f4923f013",
+    "url": "/neerajs296/music-master-neer.git/index.html"
+  },
+  {
+    "revision": "24dd2c4ca11ec186a630",
+    "url": "/neerajs296/music-master-neer.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "33a519745382b8df06ce",
+    "url": "/neerajs296/music-master-neer.git/static/js/2.ce363c69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/neerajs296/music-master-neer.git/static/js/2.ce363c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24dd2c4ca11ec186a630",
+    "url": "/neerajs296/music-master-neer.git/static/js/main.67dcdf0d.chunk.js"
+  },
+  {
+    "revision": "f90b698c17fceaaf3119",
+    "url": "/neerajs296/music-master-neer.git/static/js/runtime-main.69cc6806.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/neerajs296/music-master-neer.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
